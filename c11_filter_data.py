c3ea6b5cb8c7cd2b204370db76413885ca452f7a
Luis Jacobo <16004230@es.uveg.edu.mx>
2022-10-16 22:07:13 -0500

    Se agrega FILTER DATA

--- c11_filter_data.py
+++ c11_filter_data.py
@@ -78,7 +78,36 @@ DATA = [
 
 #List comprehensions
 def run():
+    # Language programming
     all_python_devs = [worker["name"] for worker in DATA if worker["language"] == "python"]
+    # worker representa a cada uno de los diccionarios internos
+    # [name] es la llave
+    # Creamos un ciclo for
+    
+    # Organization 
+    all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+    
+    # Filter  // Hihg order functions whit lambda functions
+    """
+    Para obtener a todos los adultos (18), se aplica un filter con un lamda,
+    posteriormente se usa map para que el resultado no imprima todo el resultado sino 
+    unicamente el nombre.
+
+    """
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    adults = list(map(lambda worker: worker["name"], adults))    
+     
+    # Crear una lista de diccionarios
+    """
+    Unir a worker con una llave más que se agrega al diccionario con pip (|)
+    Transformar al diccionario en DATA de nuestro diccioario principal con un nuevo diccioario
+    pip | sirve para unir a un diccioario con uno nuevo, con listas lo haciamos con el simbolo (+)
+    """
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+    
+    for worker in old_people:
+        print(worker)
+
 
 if __name__ == '__main__':
     run()
