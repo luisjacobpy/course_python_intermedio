0718f1a9f3c6c191df00725f59a40b22e7a7c42f
Luis Jacobo <16004230@es.uveg.edu.mx>
2022-10-26 22:52:59 -0500

    Se agrega Assert statments

--- C17_Assert statment.py
+++ C17_Assert statment.py
@@ -3,9 +3,23 @@
 
 
 """
+def divisors(num):
+    divisors = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
 
-def palindrome(string):
-        # Afirmo que la longitud del string es mayor a 0, sino corta el programa e imprime el siguiente error
-        assert len(string) > 0, "No se puede ingresar una cadena vacia"
-        return string == string[::-1]
-print(palindrome(""))
+
+def run():
+    num = input('Ingresa un número: ')
+    # [Assert  is numeric] es un metodo de las strings que lo que hace es devolver verdadero, si el string
+    # Corresponde a un numero
+    # Afirmo que lo que ingreso el usuario es un numero
+    assert num.isnumeric(), "Debes ingresar un nÃºmero"
+    print(divisors(int(num)))
+    print("TerminÃ³ mi programa")
+
+
+if __name__ == '__main__':
+    run()
